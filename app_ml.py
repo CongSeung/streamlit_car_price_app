e18e1e35a086c0811643c6c00837e63fbe07d7a0
CongSeung <leem1121@gmail.com>
2022-05-23 16:31:01 +0900

    Update app_ml.py

--- app_ml.py
+++ app_ml.py
@@ -1,5 +1,8 @@
+from tkinter import Y
 import streamlit as st
 import joblib
+import numpy as np
+import  as sk
 
 def run_ml():
     st.subheader('자동차 구매 가능 금액 예측')
@@ -19,17 +22,31 @@ def run_ml():
         gender = 0
 
     age = st.number_input('나이를 입력하세요', 0 , 100)
-    Annual_Salary = st.number_input('연봉을 입력하세요', 0)
-    Credit_Card_Debt = st.number_input('카드빚을 입력하세요', 0)
-    Net_Worth = st.number_input('자산을 입력하세요', 0)
+    salary = st.number_input('연봉을 입력하세요', 0)
+    debt = st.number_input('카드빚을 입력하세요', 0)
+    worth = st.number_input('자산을 입력하세요', 0)
 
-    new_data = np.array([gender, age, Annual_Salary, Credit_Card_Debt, Net_Worth])
-    # new_data = new_data.reshape(1, 5)
-    # new_data = X_scaler.transform(new_data)
-    # y_pred = regressor.predict(new_data)
-    # y_scaler.inverse_transform(y_pred)
+    if st.button('자동차 구매 금액 예측하기') :
+
+        # 1. 신규고객의 정보를 넘파이 어레이로 만들어준다.
+        new_data = np.array([gender, age, salary, debt, worth])
+        # 2. 머신러닝에 사용하기 위해 피터스케일링을 해야한다.
+        # 그 전에, 2차원 데이터로 바꿔주기
+        new_data = new_data.reshape(1, 5)
+        # 3. 피처 스케일링 해준다.
+        new_data = X_scaler.transform(new_data)
+        # 4. 인공지능에게 예측해달라 한다.
+        y_pred = regressor.predict(new_data)
+        # 5. 피처스케일링된 예측값을 되돌린다.
+        y_pred = y_scaler.inverse_transform(y_pred)
+        # 6. 소수점이 너무 길게 나오므로
+        result = round(y_pred[0,0])
 
 
+        st.write('이 사용자의 자동차 구매 가능 금액은 ' + str(result) + '$ 입니다.')
+
+        ### 버전확인하기 ###
+        print(sklearn.__version__)
 
 
 
